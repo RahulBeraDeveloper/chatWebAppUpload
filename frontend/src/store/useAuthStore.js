55f8c68164d6851940d92666433c07d95d1ec6d6
Rahul Bera <rahulbera3654@gmail.com>
2025-08-17 11:09:27 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatwebappupload.onrender.com" : "https://chatwebappupload.onrender.com/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatwebappupload.onrender.com" : "https://chatwebappupload.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
